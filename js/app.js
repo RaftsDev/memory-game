16207e19e567c046a43ae6e8ac49abd6e8a9e9e9
Rafael <kubr2017@gmail.com>
2018-07-15 13:58:41 -0400

    Done code with collections. Ready for testing.

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@
 //listener for create Deck
 
 var deck = document.getElementsByClassName("deck")[0];
-var match = false;
+// var match = false;
+var clickNum = 0;
 deck.addEventListener("click",flipOver);
 // var restartButton = document.getElementsByClassName("restart")[0];
 // restartButton.addEventListener("click",createDeck(4));
@@ -23,35 +24,41 @@ var flipMap = new  Map();
 
 function flipOver(evt) {
   if (flipMap.size>1) return false; //deny open more than cards
-  flipMap.set(evt.target);
-  console.log("Fliparr"+flipArr);
-  evt.target.classList.add("show","open");
-  console.log(flipArr.length+":"+evt.target.children[0].classList[1]);
-  if(flipArr.length===1){
-    return false;
-  }
-  if (flipArr.length>1) {
-    var firstCard = flipArr[0];
-    var secondCard = flipArr[1];
-    var iTagFirstCard = firstCard.children[0];
-    var symbolFirstCard = iTagFirstCard.classList[1];
-    var iTagSecondCard = secondCard.children[0];
-    var symbolSecondCard = iTagSecondCard.classList[1];
-    if (symbolFirstCard === symbolSecondCard) {
-      firstCard.classList.add("match");
-      secondCard.classList.add("match");
-      flipArr = [];
-      console.log("flipArr.length>1:"+symbolFirstCard+" "+symbolSecondCard+" array size:"+flipArr.length);
+  clickNum++;//Counting clicks
+  var cardNum = clickNum;
+  flipMap.set(cardNum,evt.target);
+  console.log("flipMap:"+flipMap.get(cardNum)+" of:"+cardNum);
+  flipMap.get(cardNum).classList.add("show","open");
+  console.log("flipMap size:"+flipMap.size+" symbol:"+flipMap.get(cardNum).children[0].classList[1]);
+  // if(flipArr.length===1){
+  //   return false;
+  // }
+   setTimeout(function(){
+     flipMap.get(cardNum).classList.remove("show","open");
+     flipMap.delete(cardNum);
+   },3000);
+  if (flipMap.size>1) {
+    // var firstCard = flipArr[0];
+    // var secondCard = flipArr[1];
+    // var iTagFirstCard = firstCard.children[0];
+    // var symbolFirstCard = iTagFirstCard.classList[1];
+    // var iTagSecondCard = secondCard.children[0];
+    // var symbolSecondCard = iTagSecondCard.classList[1];
 
+    if (flipMap.get(cardNum).children[0].classList[1] === flipMap.get(cardNum-1).children[0].classList[1]) {
+      flipMap.get(cardNum).classList.add("match");
+      flipMap.get(cardNum-1).classList.add("match");
+      flipMap.clear();
+      console.log("flipMap>1 symbol:"+flipMap.get(cardNum).children[0].classList[1]+" "+flipMap.get(cardNum-1).children[0].classList[1])+" array size:"+flipMap.size);
     }
-    else{
-      setTimeout(function(){
-        flipArr[0].classList.remove("show","open");
-        flipArr[1].classList.remove("show","open");
-        flipArr=[];
-      },3000);
-        //remove earliest card
-    }
+    // else{
+    //   // setTimeout(function(){
+    //   //   flipArr[0].classList.remove("show","open");
+    //   //   flipArr[1].classList.remove("show","open");
+    //   //   flipArr=[];
+    //   // },3000);
+    //     //remove earliest card
+    // }
   }
 
     // setTimeout(function(){
